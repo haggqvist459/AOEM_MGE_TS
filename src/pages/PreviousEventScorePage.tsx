caf5f6c7983fc4d026ccc36b731d67e7c1b726fb
haggqvist459 <48819082+haggqvist459@users.noreply.github.com>
2025-04-17 11:38:07 +0200

    basic setup

--- src/pages/PreviousEventScorePage.tsx
+++ src/pages/PreviousEventScorePage.tsx
@@ -1,9 +1,18 @@
-type Props = {}
+import { CalculatorContainer, CalculatorHeader, SubHeader, Output } from "@/components";
 
-const PreviousEventScorePage = (props: Props) => {
+
+const PreviousEventScorePage = () => {
   return (
-    <div>PreviousEventScorePage</div>
+    <CalculatorContainer>
+      <CalculatorHeader title="Previous Event Scores" showTrash={false}/>
+    </CalculatorContainer>
   )
 }
 
 export default PreviousEventScorePage;
+
+
+// each previous event score
+// contains either 7 or 2 scores
+// title field for start date
+// 
