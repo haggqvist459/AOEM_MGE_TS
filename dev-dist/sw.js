2556c9a39f275789fc5f340763024166aadcce28
haggqvist459 <48819082+haggqvist459@users.noreply.github.com>
2025-05-13 19:13:57 +0200

    functional pwa, updates to icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e8jcl443ev"
+    "revision": "0.00jsc3ed1jg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/ui/NavMenu.tsx
+++ src/components/ui/NavMenu.tsx
@@ -21,7 +21,7 @@ const NavMenu = ({ isExpanded }: Props) => {
 
   return (
     <div className="relative overflow-hidden transition-all duration-500 ease-in-out" style={{ height: isExpanded ? '36px' : '0px' }}>
-      <div className="relative w-full lg:w-1/2 xl:w-1/3">
+      <div className="relative w-full lg:w-1/2 xl:w-2/5 mx-auto">
         <div className={`flex px-3 items-center justify-start md:justify-center overflow-x-auto whitespace-nowrap space-x-4 duration-500 ease-in-out ${isExpanded ? "translate-y-0" : "-translate-y-full"
           }`}
         >
